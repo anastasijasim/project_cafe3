a61370dc7d58ec5738b84d6a17417ea6d54028f4
Anastasija <a.simdiankina1996@gmail.com>
2023-04-03 23:03:39 +0300

    add to the home

---
+++ client/tengyoku/src/page/Home/Home.tsx
@@ -0,0 +1,33 @@
+// import React, { useEffect, useState } from 'react'
+
+// import { fetchTodos } from '../../api/usersApi';
+
+// type Todo ={
+//   id:number;
+//   title:string;
+// }
+
+const Home = () => {
+  // const [todos,setTodos] = useState<Todo[]> ([]);
+  // useEffect(()=>{
+  //   fetchTodos()
+  //   .then((response)=>{
+  //     setTodos(response);
+  //     console.log (response);
+  //   })
+  // }, []);
+   return (
+    <div>
+      home
+    </div>
+  //     <ul>
+  //     {todos.map((todo)=>(
+  //       <li key={todo.id}>
+  //         {todo.title}
+  //       </li>
+  //     ))}
+  //     </ul>
+  )
+}
+
+export default Home
