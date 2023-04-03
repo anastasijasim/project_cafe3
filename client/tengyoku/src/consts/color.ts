8f7bf795c3a886727f93e4bb0c6749fc83edabfd
Anastasija <a.simdiankina1996@gmail.com>
2023-04-03 23:04:12 +0300

    add to the consts

---
+++ client/tengyoku/src/consts/color.ts
@@ -0,0 +1,1 @@
+export const coffeBrownColor = "#b4856e"
---
+++ client/tengyoku/src/consts/mediaQueries.ts
@@ -0,0 +1,7 @@
+export const screenSize = {
+    mobile: "320px",
+    tablet: "768px",
+    laptop: "1024px",
+    desktop: "2560px",
+  };
+  
